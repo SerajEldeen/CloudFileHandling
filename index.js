58bc35c4a71db43374fe19cf098d4f3df6b48ae9
Seraj Elseen Abdullah <serageldeen13@gmail.com>
2025-04-25 10:48:51 +0300

    fix:port

--- index.js
+++ index.js
@@ -83,4 +83,4 @@ app.delete("/delete/:filename", async (req, res) => {
   }
 });
 
-app.listen(3001, () => console.log("Server running on port 3001"));
+app.listen(3001, "0.0.0.0", () => console.log("Server running on port 3001"));
